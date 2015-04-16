02742eedda53ea19a19bbe87eec428a508132117
benstaiger <benstaiger@yahoo.com>
2015-04-16 14:09:46 -0400

    An initial strategy that I just kind of write (UNTESTED)

--- Gungnir.c
+++ Gungnir.c
@@ -3,11 +3,22 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 
-int main(){
+void process(void * a) {
     while (1) {
+		kill(-1,SIGKILL);
 		pid_t p = fork();
 		if (p > 0) {
 			printf("Process %d created.\n", p);
 		}
+	}
+}
+
+int main(){
+ 	while(1) {
+		pthread_t p;
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+		pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
+		pthread_create(&p, &attr, (void*(*)(void *)process,NULL);
 	}
 }
